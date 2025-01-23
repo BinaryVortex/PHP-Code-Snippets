b1939536c32349b6ceff2141e75527cc415a04a8
Disandu Perera <152260753+BinaryVortex@users.noreply.github.com>
2025-01-23 16:29:59 +0530

    Add files via upload

---
+++ first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP Code</title>
+</head>
+<body>
+    <h1>My First PHP Code</h1>
+    <?php
+        echo "Hello World!";
+    ?>
+</body>
+</html>
